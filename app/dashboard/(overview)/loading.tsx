8d18a587455bb3c1e3226c1a89471542abd13cc8
HeinZarNe <heinzarne2522005@gmail.com>
2023-11-29 20:30:19 +0630

    Finish

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import DashboardSkeleton from '../../ui/skeletons';
+
+const Page = () => {
+  return <DashboardSkeleton />;
+};
+
+export default Page;
--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,8 @@
+import { Metadata } from 'next';
 import React from 'react';
-
+export const metadata: Metadata = {
+  title: 'Customers',
+};
 const Page = () => {
   return <div>Customer</div>;
 };
